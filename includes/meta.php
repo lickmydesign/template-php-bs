1bad8c167a688f13280502bb98fd0d326d06026b
Adam O'Dwyer <adamodwyer@infinitegroup.co.uk>
2016-01-08 10:02:25 +0000

    add blank favicon, windows tiles and apple-touch-icon files, update meta to match html5 boilerplate

--- includes/meta.php
+++ includes/meta.php
@@ -1,11 +1,14 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<title><?php echo $settings['site_name']; ?> | <?php echo $page_name ?></title>
-	<meta title="description" content="<?php echo $page_description; ?>" />
+	<meta title="description" content="<?php echo $page_description; ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+
 	<link rel="stylesheet" href="css/bootstrap.min.css" />
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css" />
 	<link rel="stylesheet" href="css/font-awesome.min.css" />
