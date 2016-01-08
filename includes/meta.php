5aeba6d3c293c7180d14078700c1a78ab0e3ed4d
Adam O'Dwyer <adamodwyer@infinitegroup.co.uk>
2016-01-08 09:56:36 +0000

    add browser upgrade notice

--- includes/meta.php
+++ includes/meta.php
@@ -14,4 +14,7 @@
 	<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
+	<!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
 	<?php require_once("ga.php"); ?>
